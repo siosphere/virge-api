60d22817caabc721ce3bba3c85a4995905cc04e7
Michael Kramer <mkramer@coursehero.com>
2017-03-15 17:05:36 -0700

    Helper to create api collections

--- Virge/Api/Component/Collection.php
+++ Virge/Api/Component/Collection.php
@@ -10,8 +10,35 @@ namespace Virge\Api\Component;
  * }
  * @author Michael Kramer
  */
-class Collection extends \Virge\Core\Model {
+class Collection extends \Virge\Core\Model 
+{
     public $next;
     public $prev;
     public $rows;
+
+    public static function buildCollection(array $rows = [], int $limit = 25, int $page = 0, string $urlRoot = '/', array $queryParams = [])
+    {
+        $collection = new static();
+
+        if(count($rows) > $limit) {
+            $rows = array_splice($rows, 0, $limit);
+            $collection->setNext(static::_buildApiUrl($urlRoot, $page + 1, $queryParams));
+        }
+
+        if($page > 0) {
+            $collection->setPrev(static::_buildApiUrl($urlRoot, $page - 1, $queryParams));
+        }
+
+        $collection->setRows($rows);
+
+        return $collection;
+    }
+
+    protected static function _buildApiUrl(string $urlRoot, int $page, array $queryParams = [])
+    {
+        $queryParams['page'] = $page;
+        $queryString = http_build_query($queryParams);
+
+        return sprintf('%s?%s', $urlRoot, $queryString);
+    }
 }
